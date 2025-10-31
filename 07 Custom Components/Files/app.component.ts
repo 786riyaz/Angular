85d6b1323f12fa50e3eec45877326ec711d463cf
Riyaz Pathan <moh.riyazpathan@gmail.com>
2025-10-31 12:45:08 +0530

    Added Codes, Notes and Output for Component Topic

---
+++ 07 Custom Components/Files/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LoginComponent, SignupComponent, ProfileComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular 19';
+  name = 'Riyaz';
+}
---
+++ 07 Custom Components/Files/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-profile",
+    imports: [],
+    //   templateUrl: './profile.component.html',
+    template: `<h2>Profile Component</h2>`,
+    
+    //   styleUrl: './profile.component.css'
+    styles: [`h2 { color: blue; }`]
+})
+export class ProfileComponent { }    
